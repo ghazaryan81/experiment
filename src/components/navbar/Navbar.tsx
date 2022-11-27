a99fe85434eca5f97abf14ece90b24b34849623b
ghazaryan81 <hovhannesi.hayk.ghazaryan.81@gmail.com>
2022-11-27 11:33:03 +0400

    header semantic + BEM

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,24 +4,24 @@ import basket from "../../assets/images/basket.svg";
 
 const Navbar = ({ items }) => {
   return (
-    <header className="navbar">
-      <div className="navbar_logo">
-        <img src={Logo} alt="logo" />
+    <header className="main-header">
+      <div className="main-header__logo">
+        <img src={Logo} alt="NOOTRIS" />
       </div>
-      <div>
-        <ul className="navbar-items">
-          {items?.map((item) => (
-            <li className="navbar-items_item">
-              <a href="#">{item}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="navbar-basket">
-        <button className="navbar-basket_icon">
-          <img src={basket} alt="logo" />
+      <ul className="menu">
+        {items?.map((item) => (
+          <li className="menu__item">
+            <a href="#" className="menu__item-link">
+              {item}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="main-header__basket">
+        <button className="main-header__icon">
+          <img src={basket} alt="basket" />
         </button>
-        <div className="navbar-basket_quantity">1</div>
+        <div className="main-header__quantity">1</div>
       </div>
     </header>
   );
